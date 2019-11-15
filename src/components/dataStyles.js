88c50ff016a108444d63e2c05ed870c732703940
atakansavas <atakansavas@atakans-MBP.mshome.net>
2019-11-15 15:19:44 +0300

    style updated

--- src/components/dataStyles.js
+++ src/components/dataStyles.js
@@ -4,10 +4,10 @@ import { Circle as CircleStyle, Fill, Stroke, Style, Text } from 'ol/style';
 
 export const DefaultStyle = new Style({
     fill: new Fill({
-        color: 'rgba(116, 139, 133, 1)'
+        color: 'rgba(116, 139, 133, 0.6)'
     }),
     stroke: new Stroke({
-        color: '#319FD3',
+        color: 'white',
         width: 1
     }),
     text: new Text({
@@ -24,16 +24,16 @@ export const DefaultStyle = new Style({
 
 export const VisaFreeStyle = new Style({
     fill: new Fill({
-        color: 'rgba(158, 242, 141, 1)'
+        color: 'rgba(158, 242, 141, 0.6)'
     }),
     stroke: new Stroke({
-        color: '#319FD3',
+        color: 'white',
         width: 1
     }),
     text: new Text({
         font: '12px Calibri,sans-serif',
         fill: new Fill({
-            color: 'white'
+            color: '#000'
         }),
         stroke: new Stroke({
             color: '#fff',
